bdfd5b5a60a3d0404e0c6f0fd4fd767e165444fa
Wendi Liu <wendi.liu1988@icloud.com>
2025-09-21 19:34:24 +0000

    Add libs

---
+++ src/libs/coupledTime/subCycleCoupledTime.C
@@ -0,0 +1,118 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011 OpenFOAM Foundation
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "subCycleCoupledTime.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::subCycleCoupledTime::subCycleCoupledTime(CoupledTime& runTime, const label nCycles)
+:
+    time_(runTime),
+    index_(0),
+    total_(nCycles)
+{
+    // Could avoid 0 or 1 nCycles here on construction
+    if (nCycles > 1)
+    {
+        time_.subCycle(nCycles);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::subCycleCoupledTime::~subCycleCoupledTime()
+{
+    endSubCycle();
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::subCycleCoupledTime::status() const
+{
+    return (index_ <= total_);
+}
+
+
+bool Foam::subCycleCoupledTime::end() const
+{
+    return (index_ > total_);  // or !(status())
+}
+
+
+void Foam::subCycleCoupledTime::endSubCycle()
+{
+    if (total_ > 1)
+    {
+        time_.endSubCycle();
+    }
+
+    // If called manually, ensure status() will return false
+
+    index_ = total_ + 1;
+}
+
+
+bool Foam::subCycleCoupledTime::loop()
+{
+    const bool active = status();
+
+    if (active)
+    {
+        operator++();
+    }
+
+    return active;
+}
+
+
+// * * * * * * * * * * * * * * * Member Operators  * * * * * * * * * * * * * //
+
+Foam::subCycleCoupledTime& Foam::subCycleCoupledTime::operator++()
+{
+    if (total_ > 1)
+    {
+        time_++;
+    }
+
+    index_++;
+
+    // Register index change with Time, in case someone wants this information
+    time_.subCycleIndex(index_);
+
+    return *this;
+}
+
+
+Foam::subCycleCoupledTime& Foam::subCycleCoupledTime::operator++(int)
+{
+    return operator++();
+}
+
+
+// ************************************************************************* //
